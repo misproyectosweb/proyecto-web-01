22799edc08932b4a7e436e67d37257c9668fca81
Paulo Cesar Gomez Carmiol <paulocgomez70@gmail.com>
2023-06-05 19:13:45 -0600

    Guardando proyecto

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+
+<!-- Página principal de la iglesia Hacienda del Rey  -->
+
+<html lang="es">
+    <head>
+        <title>Inicio</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="Iglesia, Hacienda, Rey, invitar, acompañar, adorar, formar, amar, servir, enseñar, solidaridad, doctrina"/>
+        <meta name="description" content="Bienvenidos a la página web de la Iglesia Hacienda del Rey, donde encontrará noticias, articulos y mucha más información para tu crecimiento espiritual"/>                
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital@0;1&family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">                          
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">        
+        <link rel="shortcut icon" type="image/x-icon" href="imagenes/icono_iglesia.png"/>        
+        <link href="librerias/normalize.css" rel="stylesheet" type="text/css"/>                 
+        <link href="css/index.css" rel="stylesheet" type="text/css"/>        
+    </head>
+    <body>
+        <div class="contenedor">
+            
+            <!--  ***** Inicio sección encabezado ***** -->            
+            <header class="encabezado">            
+                <div class="logo">
+                    <img src="imagenes/Logo_iglesia_color.png" alt="Logo oficial de la iglesia Hacienda del Rey"/>
+                </div>                  
+            </header>
+            <!--  ***** Fin sección encabezado ***** -->
+            
+            <!--  ***** Inicio contenido principal ***** -->
+            <main class="contenido">
+                
+                <!--  ***** Inicio sección títulos ***** -->
+                <div class="textoTitulos">
+                    <img src="imagenes/bordes_pq_4.png" class="imgBorde" alt=""/>
+                    <p class="borde">"Mira que te mando, que te esfuerces y seas valiente; no temas ni desmayes,<br> porque Jehová, tu Dios, estará contigo dondequiera que vayas"<br>Josué 1:9</p>
+                </div>     
+                <!--  ***** Fin sección títulos ***** -->
+                                                
+                <!--  ***** Inicio sección menú de opciones ***** -->                
+                <nav class="menu" data-aos="fade-down" data-aos-delay="1000" data-aos-duration="1000">                                                       
+                    <a href="nuestraVision.php" class="item">                        
+                        <i class="opcion fas fa-church"></i>
+                        <h2 class="tituloItem">Quiénes somos</h2>
+                    </a>
+                    <a href="reflexiones.php" class="item">                        
+                        <i class="opcion fas fa-bible"></i>
+                        <h2 class="tituloItem">Reflexiones</h2>
+                    </a>
+                    <a href="actividades.php" class="item">                        
+                        <i class="opcion far fa-calendar-alt"></i>
+                        <h2 class="tituloItem">Actividades</h2>
+                    </a>
+                    <a href="contacto.php" class="item">                        
+                        <i class="opcion fas fa-envelope-open-text"></i>
+                        <h2 class="tituloItem">Contáctenos</h2>
+                    </a>
+                </nav>
+                <!--  ***** Fin sección menú de opciones ***** -->
+                
+                <!--  ***** Inicio sección texto de la visión ***** -->
+                <div class="imagenFondo">
+                    <div class="txtVision">                        
+                        <p>"Lorem ipsum dolor sit amet, consectetur<br> adipiscing elit, sed do eiusmod tempor incididunt<br> ut labore et dolore magna aliqua. Ut enim ad<br> minim veniam, quis nostrud exercitation<br> ullamco laboris nisi"</p>
+                    </div>
+                </div>
+                <!--  ***** Fin sección texto de la visión ***** -->
+                
+                <!--  ***** Inicio sección galería de imágenes ***** -->
+                <div class="contenedor-slideshow" id="slideshow">
+                    <div class="contenedor-imagenes">                                                
+                         <div><img src="imagenes/textobiblico_1.jpg" class="imgSlide" alt=""/></div>
+                         <div><img src="imagenes/textobiblico_2.jpg" class="imgSlide" alt=""/></div>
+                         <div><img src="imagenes/textobiblico_3.jpg" class="imgSlide" alt=""/></div>
+                         <div><img src="imagenes/textobiblico_4.jpg" class="imgSlide" alt=""/></div>                                                                                                 
+                     </div>
+                 </div>
+                <!--  ***** Fin sección galería de imágenes ***** -->
+                
+                <!--  ***** Inicio efecto de onda ***** -->
+                <div class="wave">
+                    <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+                        <path d="M0.00,49.98 C119.35,151.47 320.26,-102.13 500.00,49.98 L500.00,150.00 L-1.41,154.44 Z" style="stroke: none; fill: #003958;"></path>
+                    </svg>
+                </div>
+                <!--  ***** Fin efecto de onda ***** -->
+            </main>
+            
+            <!-- ***** Inicio sección pie de página ***** -->
+            <footer class="contenedorPiepagina">
+                                                                
+                <!-- ***** Inicio sección logo iglesia ***** -->
+                <div class="columna-logo" data-aos="zoom-in-down" data-aos-duration="1500">
+                    <img src="imagenes/Logo_iglesia_blanco.png" alt=""/>
+                </div>                
+                <!-- ***** Fin sección logo iglesia ***** -->
+                
+                <!-- ***** Inicio sección conociendo la iglesia ***** -->
+                <div class="columna-menu" data-aos="zoom-in-down" data-aos-duration="1500">
+                    <h2>Conozca nuestra iglesia:</h2>
+                    <ul>
+                        <li><a href="nuestraVision.php">Quiénes somos</a></li>
+                        <li><a href="reflexiones.php">Reflexiones</a></li>
+                        <li><a href="actividades.php">Actividades</a></li>
+                        <li><a href="contacto.php">Contáctenos</a></li>
+                    </ul>                                                                                                
+                </div>
+                <!-- ***** Fin sección conociendo la iglesia ***** -->
+                
+                <!-- ***** Inicio sección redes sociales ***** -->
+                <div class="columna-redes" data-aos="zoom-in-down" data-aos-duration="1500">
+                    <h2>Siganos en redes por:</h2>
+                    <div class="redes">
+                        <i class="icono fab fa-facebook-f"></i>
+                        <a href="#"><label>Facebook</label></a>
+                    </div>
+                    <div class="redes">
+                        <i class="icono fab fa-whatsapp"></i>                        
+                        <a href="#"><label>Whatsapp</label></a>
+                    </div>
+                    <div class="redes">
+                        <i class="icono fab fa-instagram"></i>
+                        <a href="#"><label>Instagram</label></a>
+                    </div>
+                </div>
+                <!-- ***** Fin sección redes sociales ***** -->
+                
+                <div class="columna-contacto" data-aos="zoom-in-down" data-aos-duration="1500">
+                    <h2>Información de contacto:</h2>
+                    <div class="datos">
+                        <i class="icono fab fa-waze"></i>                        
+                        <a href="contacto.html">Moravia, Romeral, diagonal 53</a>                        
+                    </div>
+
+                    <div class="datos">
+                        <i class="icono fas fa-mobile-alt"></i>
+                        <label>(+506)&nbsp;XXXX-XXXX</label>
+                    </div>
+
+                    <div class="datos">
+                        <i class="icono fas fa-envelope-open-text"></i>
+                        <label>Lorem_ipsum@sitamet.com</label>
+                    </div>
+                </div>
+                <!-- ***** Fin sección datos de contacto ***** -->
+                
+                <!-- ***** Inicio sección datos de copyright ***** -->
+                <div class="contenedorInformacion">
+                    <div class="barraInfo">
+                        <i class="far fa-copyright"></i>
+                        <label>&nbsp;2022 Todos los Derechos Reservados&nbsp;&nbsp;-&nbsp;&nbsp;Iglesia Hacienda del Rey</label>
+                    </div>
+                </div>
+                <!-- ***** Fin sección datos de copyright *****-->   
+                
+            </footer>
+            <!-- ***** Inicio sección pie de página ***** -->                         
+        </div>
+                                                                        
+        <!-- ***** Archivos javascript y CDN's ***** -->                            
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>                              
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>        
+        <script src="javascript/galeriaImagenes.js" type="text/javascript"></script>
+                
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
